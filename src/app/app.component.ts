67d1991bf3af61ad5a5fc7f0827db9a3292cab9f
Tarif Halabi <tarif.halabi@gmail.com>
2018-08-01 14:17:25 -0400

    Update AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    version: string = 'TODO need to call the rest endpoint';
+    title = 'app';
 }
