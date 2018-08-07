1d50e827d17b2a90001bcb0e734bbce472ef510f
Tarif Halabi <tarif.halabi@gmail.com>
2018-08-06 20:57:31 -0400

    Fix to determine component name in mimified files ie --prod build.

--- src/app/util/CustomRouteReuseStrategy.ts
+++ src/app/util/CustomRouteReuseStrategy.ts
@@ -1,4 +1,5 @@
 import { ActivatedRouteSnapshot, RouteReuseStrategy, DetachedRouteHandle } from "@angular/router";
+import { GenericCrudComponent } from "../generic-crud/generic-crud.component";
 
 export class DefaultRouteReuseStrategy implements RouteReuseStrategy {
     shouldDetach(route: ActivatedRouteSnapshot): boolean { return false; }
@@ -12,22 +13,22 @@ export class DefaultRouteReuseStrategy implements RouteReuseStrategy {
 
 export class CustomRouteReuseStrategy extends DefaultRouteReuseStrategy  {
 
-    readonly DO_NOT_REUSE_COMPONENTS: Array<string> = ['GenericCrudComponent'];
+    readonly DO_NOT_REUSE_COMPONENTS: Array<any> = [GenericCrudComponent];
 
     // TODO not sure what is future and current
     // The output from the log stmts below does not make sense
     shouldReuseRoute(future: ActivatedRouteSnapshot, current: ActivatedRouteSnapshot): any {
         // console.log('future: ', future, ', current: ', current);
-        if (current && current.routeConfig && current.routeConfig.component && current.routeConfig.component.name) {
-            console.log('current component name: ', current.routeConfig.component.name)
+        if (current && current.routeConfig && current.routeConfig.component) {
+            console.log('current component: ', current.routeConfig.component);
         }
-        if (future && future.routeConfig && future.routeConfig.component && future.routeConfig.component.name) {
-            console.log('future component name: ', future.routeConfig.component.name)
+        if (future && future.routeConfig && future.routeConfig.component) {
+            console.log('future component: ', future.routeConfig.component)
         }
 
-        let componentName: string  = future.component && (<any>future.component).name;
-        console.log('future component name is in DO_NOT_REUSE_COMPONENTS: ', this.DO_NOT_REUSE_COMPONENTS.includes(componentName));
-        return super.shouldReuseRoute(future, current) && ! this.DO_NOT_REUSE_COMPONENTS.includes(componentName);
+        //let componentName: string  = future.component && (<any>future.component).name;
+        console.log('future component name is in DO_NOT_REUSE_COMPONENTS: ', this.DO_NOT_REUSE_COMPONENTS.includes(future.component));
+        return super.shouldReuseRoute(future, current) && ! this.DO_NOT_REUSE_COMPONENTS.includes(future.component);
     }
 }
 
